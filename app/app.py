9994210733e9056c16f0be02b8e3e18b8f0b8c2b
Ibtisam-Mohammad <63063432+Ibtisam-Mohammad@users.noreply.github.com>
2022-04-18 15:03:58 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,43 +9,25 @@ import pickle
 import pydeck as pdk
 from pydeck.types import String
 
-st.set_page_config(
-     page_title="Indian Housing Data",
-     page_icon="🧊",
-     layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "# This is a header. This is an *extremely* cool app!"
-     }
- )
-st.markdown(
-  """
-  <style>
-  [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {width: 120px;}
-  </style>
-  """,
-  unsafe_allow_html=True,)
-rad = st.sidebar.radio('Navigation',['Home','Search'])
 
 
 
 
-if rad =='Search':
-  path='app/PartA_df/'
-  files=os.listdir(path)
-
-  @st.cache(allow_output_mutation=True,suppress_st_warning=True)
-  def ind(i):
-    return pd.read_csv(path+i).drop(['State Code','District Code'],axis=1)
 
 
-  for j,i in enumerate(files):
+path='app/PartA_df/'
+files=os.listdir(path)
+
+@st.cache(allow_output_mutation=True,suppress_st_warning=True)
+def ind(i):
+   return pd.read_csv(path+i).drop(['State Code','District Code'],axis=1)
+
+
+for j,i in enumerate(files):
     nam=i.split('.')[0]
     globals()[nam]=ind(i)
 
-  dic_={
+dic_={
   "Ameneties":df_assets,
   "Bathroom type":df_bath,
   "Condition of house":df_condn,
@@ -63,25 +45,25 @@ if rad =='Search':
   "Material of Wall":df_wall,
   "Waste Outlet":df_waste,
   "Water source and location":df_water
-  }
+}
 
 #   @st.cache(allow_output_mutation=True,suppress_st_warning=True)
 #   @st.cache()
 
-  def fil(dic_):
+def fil(dic_):
     for i,j in dic_.items():
       if i=='Ameneties':
         B=j.iloc[:,:2]
       B=pd.concat([B,j.iloc[:,2:]],axis=1,join='inner')
-    B['temp2']=B['District Name']+' '+B['State Name']
+#     B['temp2']=B['District Name']+' '+B['State Name']
 #     lat_long=pd.read_csv('scrapped_lat_long/lat_long.csv')
 #     B=pd.merge(B,lat_long,how='inner',left_on='temp2',right_on='Unnamed: 0')
 #     B.drop(['temp2','Unnamed: 0'],axis=1,inplace=True)
     return B
-  B=fil(dic_)
-#   lat_long=pd.read_csv('app/scrapped_lat_long/lat_long.csv')
-#   st.write(lat_long)
-  st.write(B[:1000])
+B=fil(dic_)
+lat_long=pd.read_csv('app/scrapped_lat_long/lat_long.csv')
+st.write(lat_long)
+st.write(B[:1000])
   
 
 
